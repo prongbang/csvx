f66a173412efc4a05311b143a8ecba462ecd3793
prongbang <prongbang@gmail.com>
2023-04-23 20:15:42 +0700

    Add unit test common

---
+++ common_test.go
@@ -0,0 +1,21 @@
+package csvx_test
+
+import (
+	"github.com/prongbang/csvx"
+	"reflect"
+	"testing"
+)
+
+func TestIsFloat(t *testing.T) {
+	ty := reflect.TypeOf(float64(0))
+	if !csvx.IsFloat(ty) {
+		t.Error("Is Not Float")
+	}
+}
+
+func TestF64ToString(t *testing.T) {
+	f := 6.4
+	if csvx.F64ToString(f) != "6.4" {
+		t.Error("Actual is not eq", f)
+	}
+}
