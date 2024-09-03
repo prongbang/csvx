03bba2a18ffe1a4a40690f899830b776b8f5e178
prongbang <prongbang@gmail.com>
2024-09-03 22:03:09 +0700

    poc: add function convert to csv

--- convert.go
+++ convert.go
@@ -1,8 +1,10 @@
 package csvx
 
 import (
+	"encoding/csv"
 	"fmt"
 	"reflect"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -32,88 +34,270 @@ func Format(cell []string) string {
 //	"2","N2"
 func Convert[T any](data []T, ignoreDoubleQuote ...bool) string {
 	size := len(data)
-	if size > 0 {
-
-		// Config format value
-		valueFormatCore := "%v"
-		valueFormat := "\"%v\""
-		if len(ignoreDoubleQuote) > 0 {
-			valueFormat = valueFormatCore
-		}
-
-		// Initialize the element
-		var headers []string
-		rows := make([][]string, size)
-
-		// Mapping
-		sheets := []string{}
-		for r, d := range data {
-			el := reflect.ValueOf(&d).Elem()
-
-			colsRaw := el.NumField()
-			cols := 0
-			for c := 0; c < colsRaw; c++ {
-				_, fOk := fieldLookup[T](d, c)
-				_, iOk := indexLookup[T](d, c)
-				if !fOk || !iOk {
-					continue
-				}
-				cols++
-			}
-			if headers == nil {
-				headers = make([]string, cols)
-			}
-			if len(rows[r]) == 0 {
-				rows[r] = make([]string, cols)
-			}
-
-			for c := 0; c < colsRaw; c++ {
-				value := el.Field(c)
-				field, fOk := fieldLookup[T](d, c)
-				index, iOk := indexLookup[T](d, c)
-				if !fOk || !iOk {
-					continue
-				}
-
-				if i, err := strconv.Atoi(index); err == nil {
-					if r == 0 {
-						headers[i-1] = fmt.Sprintf(valueFormat, field)
-					}
-					if IsFloat(value.Type()) {
-						rows[r][i-1] = fmt.Sprintf(valueFormat, F64ToString(value.Float()))
-					} else {
-						nValue := ""
-						if IsPointer(value.Type()) {
-							if value.Elem().IsValid() {
-								nValue = RemoveDoubleQuote(fmt.Sprintf(valueFormatCore, value.Elem()))
-							}
-						} else {
-							nValue = RemoveDoubleQuote(fmt.Sprintf(valueFormatCore, value))
-						}
-						rows[r][i-1] = fmt.Sprintf(valueFormat, nValue)
-					}
-				}
-			}
-
-			// Convert array to csv format
-			if len(sheets) == 0 {
-				sheets = append(sheets, Format(headers))
-			}
-			sheets = append(sheets, Format(rows[r]))
-		}
-
-		// Add enter end line
-		result := strings.Join(sheets, "\n")
-		return result
+	if size == 0 {
+		return ""
 	}
 
-	return ""
+	// Config format value
+	valueFormatCore := "%v"
+	valueFormat := "\"%v\""
+	if len(ignoreDoubleQuote) > 0 {
+		valueFormat = valueFormatCore
+	}
+
+	// Initialize the element
+	var headers strings.Builder
+	rows := make([]strings.Builder, size)
+
+	first := data[0]
+	fel := reflect.ValueOf(&first).Elem()
+	numField := fel.NumField()
+	for c := 0; c < numField; c++ {
+		field, fOk := headerLookup[T](first, c)
+		index, iOk := noLookup[T](first, c)
+		if fOk || iOk {
+			fmt.Println(field, index)
+		}
+	}
+
+	// Mapping
+	sheets := []string{}
+	for r, d := range data {
+		el := reflect.ValueOf(&d).Elem()
+
+		colsRaw := el.NumField()
+		for c := 0; c < colsRaw; c++ {
+			value := el.Field(c)
+			field, fOk := headerLookup[T](d, c)
+			no, iOk := noLookup[T](d, c)
+			if !fOk || !iOk {
+				continue
+			}
+
+			if _, err := strconv.Atoi(no); err == nil {
+				isNotLast := c < colsRaw-1
+				if r == 0 {
+					headers.WriteString(fmt.Sprintf(valueFormat, field))
+					if isNotLast {
+						headers.WriteString(",")
+					}
+				}
+				if IsFloat(value.Type()) {
+					rows[r].WriteString(fmt.Sprintf(valueFormat, F64ToString(value.Float())))
+				} else {
+					nValue := ""
+					if IsPointer(value.Type()) {
+						if value.Elem().IsValid() {
+							nValue = RemoveDoubleQuote(fmt.Sprintf(valueFormatCore, value.Elem()))
+						}
+					} else {
+						nValue = RemoveDoubleQuote(fmt.Sprintf(valueFormatCore, value))
+					}
+					rows[r].WriteString(fmt.Sprintf(valueFormat, nValue))
+				}
+
+				if isNotLast {
+					rows[r].WriteString(",")
+				}
+			}
+		}
+
+		// Convert array to csv format
+		if len(sheets) == 0 {
+			sheets = append(sheets, headers.String())
+		}
+		sheets = append(sheets, rows[r].String())
+	}
+
+	// Add enter end line
+	result := strings.Join(sheets, "\n")
+	return result
 }
 
-func fieldLookup[T any](d T, c int) (string, bool) {
+func ConvertToCSV[T any](data []T) string {
+	if len(data) == 0 {
+		return ""
+	}
+
+	// Use reflection to get the type of the struct
+	t := reflect.TypeOf(data[0])
+
+	// Create a slice to hold the fields and their `no` tags
+	fields := make([]struct {
+		field reflect.StructField
+		no    int
+	}, t.NumField())
+
+	// Populate the fields slice
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		noTag := field.Tag.Get("no")
+		no := 0
+		fmt.Sscanf(noTag, "%d", &no)
+		fields[i] = struct {
+			field reflect.StructField
+			no    int
+		}{field, no}
+	}
+
+	// Sort fields by `no` tag
+	sort.Slice(fields, func(i, j int) bool {
+		return fields[i].no < fields[j].no
+	})
+
+	// Create a CSV writer
+	var csvBuilder strings.Builder
+	csvWriter := csv.NewWriter(&csvBuilder)
+
+	// Write headers
+	headers := make([]string, len(fields))
+	for i, f := range fields {
+		headers[i] = f.field.Tag.Get("header")
+	}
+	csvWriter.Write(headers)
+
+	// Write data
+	for _, record := range data {
+		values := make([]string, len(fields))
+		val := reflect.ValueOf(record)
+		for i, f := range fields {
+			values[i] = fmt.Sprintf("%v", val.FieldByName(f.field.Name))
+		}
+		csvWriter.Write(values)
+	}
+
+	csvWriter.Flush()
+	return csvBuilder.String()
+}
+
+func ConvertToCSV2[T any](data []T) string {
+	if len(data) == 0 {
+		return ""
+	}
+
+	// Use reflection to get the type of the struct
+	t := reflect.TypeOf(data[0])
+
+	// Initialize arrays to hold headers and field names in the correct order
+	headers := make([]string, t.NumField())
+	fieldNames := make([]string, t.NumField())
+
+	// Populate the arrays according to the `no` tag
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		noTag := field.Tag.Get("no")
+		no := 0
+		fmt.Sscanf(noTag, "%d", &no)
+		// Place headers and fields in the correct position based on `no` tag
+		headers[no-1] = field.Tag.Get("header")
+		fieldNames[no-1] = field.Name
+	}
+
+	// Create a CSV writer
+	var csvBuilder strings.Builder
+	csvWriter := csv.NewWriter(&csvBuilder)
+
+	// Write headers
+	csvWriter.Write(headers)
+
+	// Write data
+	for _, record := range data {
+		values := make([]string, len(fieldNames))
+		val := reflect.ValueOf(record)
+		for i, fieldName := range fieldNames {
+			values[i] = fmt.Sprintf("%v", val.FieldByName(fieldName))
+		}
+		csvWriter.Write(values)
+	}
+
+	csvWriter.Flush()
+	return csvBuilder.String()
+}
+
+func TryConvert[T any](data []T, ignoreDoubleQuote ...bool) string {
+	size := len(data)
+	if size == 0 {
+		return ""
+	}
+
+	// Config format value
+	valueFormatCore := "%v"
+	valueFormat := "\"%v\""
+	if len(ignoreDoubleQuote) > 0 {
+		valueFormat = valueFormatCore
+	}
+
+	// Initialize the element
+	var headers []string
+	rows := make([][]string, size)
+
+	// Mapping
+	sheets := []string{}
+	for r, d := range data {
+		el := reflect.ValueOf(&d).Elem()
+
+		colsRaw := el.NumField()
+		cols := 0
+		for c := 0; c < colsRaw; c++ {
+			_, fOk := headerLookup[T](d, c)
+			_, iOk := noLookup[T](d, c)
+			if !fOk || !iOk {
+				continue
+			}
+			cols++
+		}
+
+		if headers == nil {
+			headers = make([]string, cols)
+		}
+		if len(rows[r]) == 0 {
+			rows[r] = make([]string, cols)
+		}
+
+		for c := 0; c < colsRaw; c++ {
+			value := el.Field(c)
+			field, fOk := headerLookup[T](d, c)
+			index, iOk := noLookup[T](d, c)
+			if !fOk || !iOk {
+				continue
+			}
+
+			if i, err := strconv.Atoi(index); err == nil {
+				if r == 0 {
+					headers[i-1] = fmt.Sprintf(valueFormat, field)
+				}
+				if IsFloat(value.Type()) {
+					rows[r][i-1] = fmt.Sprintf(valueFormat, F64ToString(value.Float()))
+				} else {
+					nValue := ""
+					if IsPointer(value.Type()) {
+						if value.Elem().IsValid() {
+							nValue = RemoveDoubleQuote(fmt.Sprintf(valueFormatCore, value.Elem()))
+						}
+					} else {
+						nValue = RemoveDoubleQuote(fmt.Sprintf(valueFormatCore, value))
+					}
+					rows[r][i-1] = fmt.Sprintf(valueFormat, nValue)
+				}
+			}
+		}
+
+		// Convert array to csv format
+		if len(sheets) == 0 {
+			sheets = append(sheets, Format(headers))
+		}
+		sheets = append(sheets, Format(rows[r]))
+	}
+
+	// Add enter end line
+	result := strings.Join(sheets, "\n")
+	return result
+}
+
+func headerLookup[T any](d T, c int) (string, bool) {
 	return reflect.ValueOf(d).Type().Field(c).Tag.Lookup("header")
 }
 
-func indexLookup[T any](d T, c int) (string, bool) {
+func noLookup[T any](d T, c int) (string, bool) {
 	return reflect.ValueOf(d).Type().Field(c).Tag.Lookup("no")
 }
--- convert_test.go
+++ convert_test.go
@@ -1,7 +1,11 @@
 package csvx_test
 
 import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
 	"github.com/prongbang/csvx"
+	"log"
 	"testing"
 )
 
@@ -56,6 +60,111 @@ func BenchmarkConvert(b *testing.B) {
 		// Then
 		if csv != expected {
 			b.Error("Convert error:", csv)
+		}
+	}
+}
+
+/*
+BenchmarkTryConvert
+BenchmarkTryConvert-10    	  234032	      5198 ns/op
+*/
+func BenchmarkTryConvert(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// Given
+		m := []MyStruct{{ID: 1, Name: "N1"}, {ID: 2, Name: "N2"}}
+		expected := `"ID","Name Space"
+"1","N1"
+"2","N2"`
+
+		// When
+		csv := csvx.TryConvert[MyStruct](m)
+
+		// Then
+		if csv != expected {
+			b.Error("Convert error:", csv)
+		}
+	}
+}
+
+/*
+BenchmarkConvertToCSV
+BenchmarkConvertToCSV-10    	  584448	      2069 ns/op
+*/
+func BenchmarkConvertToCSV(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// Given
+		m := []MyStruct{{ID: 1, Name: "N1"}, {ID: 2, Name: "N2"}}
+		expected := `ID,Name Space
+1,N1
+2,N2
+`
+
+		// When
+		csv := csvx.ConvertToCSV[MyStruct](m)
+
+		// Then
+		if csv != expected {
+			b.Error("Convert error:", csv, expected)
+		}
+	}
+}
+
+/*
+BenchmarkConvertToCSV2
+BenchmarkConvertToCSV2-10    	  642216	      1853 ns/op
+*/
+func BenchmarkConvertToCSV2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// Given
+		m := []MyStruct{{ID: 1, Name: "N1"}, {ID: 2, Name: "N2"}}
+		expected := `ID,Name Space
+1,N1
+2,N2
+`
+
+		// When
+		csv := csvx.ConvertToCSV2[MyStruct](m)
+
+		// Then
+		if csv != expected {
+			b.Error("Convert error:", csv, expected)
+		}
+	}
+}
+
+/*
+BenchmarkWriter
+BenchmarkWriter-10    	 1936059	       581.2 ns/op
+*/
+func BenchmarkWriter(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		// Given
+		m := []MyStruct{{ID: 1, Name: "N1"}, {ID: 2, Name: "N2"}}
+		expected := `ID,Name Space
+1,N1
+2,N2
+`
+
+		// When
+		var buffer bytes.Buffer
+		w := csv.NewWriter(&buffer)
+
+		// Write headers
+		headers := []string{"ID", "Name Space"}
+
+		w.Write(headers)
+		for _, record := range m {
+			row := []string{fmt.Sprint(record.ID), record.Name}
+			if err := w.Write(row); err != nil {
+				log.Fatalln("error writing record to file", err)
+			}
+		}
+		w.Flush()
+		csvs := buffer.String()
+
+		// Then
+		if csvs != expected {
+			b.Error("Convert error:", csvs)
 		}
 	}
 }
